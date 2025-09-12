a3420b187f19b90b226998c6f8cd024dfbdfd7a0
Eymenonar <157057667+Eymenonar@users.noreply.github.com>
2025-09-12 17:21:05 +0300

    first commit

---
+++ src/main/java/com/onar/eymen/techMarket/TechMarketApplication.java
@@ -0,0 +1,12 @@
+package com.onar.eymen.techMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechMarketApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TechMarketApplication.class, args);
+  }
+}
---
+++ src/test/java/com/onar/eymen/techMarket/TechMarketApplicationTests.java
@@ -0,0 +1,11 @@
+package com.onar.eymen.techMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechMarketApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
